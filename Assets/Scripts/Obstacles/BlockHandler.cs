a736c405389bfbbb160454b4ae38bfdc5ca88f43
minh.phamquang <phamquangminhex@gmail.com>
2019-05-05 22:42:25 +0700

    - Add Spawn base on music
    Use single channel to get a correct result (Piano only, bass only,...)

--- Assets/Scripts/Obstacles/BlockHandler.cs
+++ Assets/Scripts/Obstacles/BlockHandler.cs
@@ -11,10 +11,53 @@ public class BlockHandler : MonoBehaviour
 
     CameraManager cameraManager = null;
 
+    // Use to spawn on beat
+    public float spawnThrehold = 0.002f;
+    public int fromFrequency = 700;
+    public int toFrequency = 800;
+    public FFTWindow ffTWindow;
+
+    private float[] samples = new float[1024];
+    // end
+
+    float spawnTime = 2.0f;
+    float timeCount = 0.0f;
+
     void Awake()
     {
         block = this.transform.GetChild(0).gameObject;
         cameraManager = GameObject.Find("Main Camera").GetComponent<CameraManager>();
+    }
+
+    void Update() {
+        timeCount += Time.deltaTime;
+        
+        resetFrequency();
+        AudioListener.GetSpectrumData(samples, 0, ffTWindow);
+
+        if(timeCount >= spawnTime){
+            if(checkFrequency()) {
+                Spawn();
+                timeCount = 0;
+            }
+        }
+       
+    }
+
+    bool checkFrequency() {
+        for(int i = fromFrequency; i < toFrequency; i++) {
+           if(samples[i] > spawnThrehold) {
+               return true;
+           }
+        }
+
+        return false;
+    }
+
+    void resetFrequency() {
+        for(int i = fromFrequency; i < toFrequency; i++) {
+            samples[i] = 0;
+        }
     }
 
     public void Spawn() {
